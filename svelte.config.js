1f35c9b34d4d2d890ec175e0816bad728edd099a
Kaleb Ercanbrack <twitcherc@gmail.com>
2021-08-10 15:54:13 -0600

    build for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import preprocess from 'svelte-preprocess';
-import staticAdapter from '@sveltejs/adapter-static';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: staticAdapter()
+		adapter: netlifyAdapter()
 	},
 
 	preprocess: [
